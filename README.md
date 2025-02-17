623c24aff49906e83e73c64185c8ee2b14b57b11
SamFarFar <sfarzamfar@yahoo.com>
2025-02-16 23:58:16 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Credits
+
+Tower model:
+"Kickelhahn Tower" (https://skfb.ly/5i3g2e10ba) by 3DHaupt is licensed under CC Attribution-NonCommercial-NoDerivs (http://creativecommons.org/licenses/by-nc-nd/4.0/).
---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect, useRef } from 'react'
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+
+function App() {
+  const mountRef = useRef(null);
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const cam = new THREE.PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+    );
+    cam.position.z = 96;
+
+    const canvas = document.getElementById('threeJsCanvas');
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias:true, // 3D objects look smooth
+    });
+
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    const ambLight = new THREE.AmbientLight(0xffffff,1);
+    ambLight.castShadow = true;
+    scene.add(ambLight);
+
+    // const spotLight = new THREE.SpotLight(0xffffff,1);
+    // spotLight.castShadow = true;
+    // spotLight.position.set(0,64,32);
+    //  scene.add(spotLight);
+
+    // const boxGeometry = new THREE.BoxGeometry(16,16,16);
+    // const boxMaterial = new THREE.MeshNormalMaterial();
+    // const boxMesh = new THREE.Mesh(boxGeometry,boxMaterial);
+    // scene.add(boxMesh);
+    const loader = new GLTFLoader();
+
+    loader.load('/assets/tower/scene.gltf', 
+      function(gltf){
+      object = gltf.scene;
+     console.log
+      scene.add(object);
+    }, function(xhr){
+      console.log((xhr.loaded/xhr.total *100) + "% Complete.")
+    },function(error){
+      console.log("HI");
+      console.error(error);
+    });
+
+    const animate = () => {
+      // boxMesh.rotation.x += 0.01;
+      // boxMesh.rotation.y += 0.01;
+
+      renderer.render(scene,cam);
+      window.requestAnimationFrame(animate);
+    };
+animate();
+
+  },[]);
+
+  return (
+    <div className='App'>
+      <canvas id="threeJsCanvas">
+      </canvas>
+    </div>
+  );
+}
+
+export default App
